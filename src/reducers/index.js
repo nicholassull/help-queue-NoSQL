68737f0006d7129499ad48dcea2653ea84603723
Nick Sullivan <nicholassull@gmail.com>
2022-06-20 19:58:22 -0700

    Update reducers/index.js to use firestore
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Anastasia Han
    <anastasia.han@yahoo.com>
    Coauthored by:  Wajma Niazi  <w1niazi@hotmail.com>
    Co-authored-by: Wajma Niazi <WNiazi@users.noreply.github.com>
    Co-authored-by: Cesar Lopez <maclopez88-0@users.noreply.github.com>
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
+
 
 //combineReducers() takes an object as an argument. That object contains key-value pairs. 
 //The key represents the state slice while the value represents the reducer that handles actions related to that state slice.
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainTicketList: ticketListReducer
+  mainTicketList: ticketListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
