aa2744bcde8d578450236d1d58d3ce52a0e5c049
Nick Sullivan <nicholassull@gmail.com>
2022-06-21 18:47:30 -0700

    Update TicketList to use Firestore
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Anastasia Han
    <anastasia.han@yahoo.com>
    Coauthored by:  Wajma Niazi  <w1niazi@hotmail.com>

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,27 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ticket from "./Ticket";
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 
 function TicketList(props){
-  return (
-    <React.Fragment>
-      <hr/>
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket
-          whenTicketClicked = { props.onTicketSelection }
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          formattedWaitTime={ticket.formattedWaitTime}
-          id={ticket.id}
-          key={ticket.id}/>
-      )}
-    </React.Fragment>
-  );
-}
+  //get useFirestoreConnect() hook from react-redux-firebase.
+  useFirestoreConnect([
+    { collection: 'tickets'}
+  ]);
+
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  if (isLoaded(tickets)) {
+    return (
+      <React.Fragment>
+        <hr/>
+        {tickets.map((ticket) =>
+          <Ticket
+            whenTicketClicked = { props.onTicketSelection }
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id}/>
+        )}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
+};
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
+  //ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
