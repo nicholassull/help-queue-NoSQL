cf1332ffbbe1b5cfbdf23569fbb1bb42eb3f7bd4
Nick Sullivan <nicholassull@gmail.com>
2022-06-26 11:00:30 -0700

    update index.js to us firebase/auth

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import rootReducer from './reducers';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
 import firebase from "./firebase";
+import 'firebase/auth';
 
 //Instantiates the redux store using our ticket reducer.
 const store = createStore(rootReducer);
@@ -16,7 +17,8 @@ const store = createStore(rootReducer);
 const rrfProps = {
   firebase,
   config: {
-    userProfile: "users"
+    userProfile: "users",
+    useFirestoreForProfile: true,
   },
   dispatch: store.dispatch,
   createFirestoreInstance
