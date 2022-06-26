4cbdf8c65c3bc060eba84ba66296b761bc9f4add
Nick Sullivan <nicholassull@gmail.com>
2022-06-26 10:40:49 -0700

    update App.js to use react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Header from "./Header";
+import Signin from "./Signin";
 import TicketControl from "./TicketControl";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App(){
-  return ( 
-    <React.Fragment>
+  return (
+    <Router>
       <Header />
-      <TicketControl />
-    </React.Fragment>
+      {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+        <TicketControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
