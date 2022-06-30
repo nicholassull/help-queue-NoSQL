7f161d74592536855671d477a2107c0bfb0f35a3
Nick Sullivan <nicholassull@gmail.com>
2022-06-29 18:56:58 -0700

    add styled-components to project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24;
+  text-align: center;
+  color: grey;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: aliceblue;
+`;
+
+const StyledListItem = styled.li`
+  display: inline;
+  margin-right: 20px;
+  
+`;
 
 function Header(){
   return (
-    <React.Fragment>
-      <h1>Help Queue</h1>
+    <StyledWrapper>
+      <HelpQueueHeader>
+        Help Queue
+      </HelpQueueHeader>
       <ul>
-        <li>
+        <StyledListItem>
           <Link to="/">Home</Link>
-        </li>
-        <li>
+        </StyledListItem>
+        <StyledListItem>
           <Link to="/signin">Sign In</Link>
-        </li>
+        </StyledListItem>
       </ul>
-    </React.Fragment>
+    </StyledWrapper>
   );
 }
 
--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,8 +1,6 @@
 import React from "react";
-// import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
-// import Moment from 'moment';
 import { useFirestore } from 'react-redux-firebase';
 
 function NewTicketForm(props){
